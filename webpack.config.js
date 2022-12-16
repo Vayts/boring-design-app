fdc49dbb67e74616775a6f3815640ac89d9e2ba9
Shevchenko Aleksandr <54949928+Vayts@users.noreply.github.com>
2022-12-16 16:33:12 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ const babelOptions = (presets) => {
 	const opts = {
 		presets: [
 			'@babel/preset-env',
+			'@babel/preset-react',
 		],
 		plugins: [
 			'@babel/plugin-proposal-class-properties',
